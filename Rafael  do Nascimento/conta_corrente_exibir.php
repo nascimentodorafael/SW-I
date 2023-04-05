36663b398b198b3b211f78ca87c7ebc81e5efd9a
nascimentodorafael <107429969+nascimentodorafael@users.noreply.github.com>
2023-04-05 08:19:49 -0300

    Add files via upload

---
+++ Rafael  do Nascimento/conta_corrente_exibir.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exibir Conta Corrente</title>
+</head>
+<body>
+    <?php
+        echo "<h3>CONTROLE BANCÁRIO - CADASTRO DE CONTA CORRENTE</h3>";
+    ?>
+</body>
+</html>
