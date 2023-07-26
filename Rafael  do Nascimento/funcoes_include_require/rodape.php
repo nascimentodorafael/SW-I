4223c2cc7dd2a6a92bef70f84bcdedf27b42a1aa
nascimentodorafael <107429969+nascimentodorafael@users.noreply.github.com>
2023-07-26 07:54:35 -0300

    Delete Rafael  do Nascimento directory

--- Rafael  do Nascimento/funcoes_include_require/rodape.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo "<h5>Website desenvolvido por <strong>SeuNome</strong> - &
-    copy& 2023 </h5>";
-    echo "<em>Todos os direitos reservados</em>";
-    echo "<p style='color:BlueViolet;font-family:impact;font-size:20pxwidth=15px'><marquee
-    behavior=alternate>Seja bem-vindo</marquee></p>";
-    
-?>
--- Rafael  do Nascimento/sessao/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-ECHO "<a href='index.php'>Voltar</a>";
-?>
